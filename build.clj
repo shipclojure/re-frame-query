52e4b5b1f0d1c5dc73b187dfad86b955a9c94a17
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2026-04-03 13:20:43 +0300

    chore: Bump release to 0.3.1

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
    [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.shipclojure/re-frame-query)
-(def version "0.3.0")
+(def version "0.3.1")
 (def class-dir "target/classes")
 (def jar-file (format "target/%s-%s.jar" lib version))
 
